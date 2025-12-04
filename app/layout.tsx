6789aa7db67b2f763c4a73aaa127126488acc0a8
Dickson Boateng <dicksonboateng67@gmail.com>
2025-12-04 09:13:51 +0000

    Initialize Nextjs project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
